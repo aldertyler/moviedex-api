3b3e2805f65e2c1222ec47c40b227bea3e7c60de
Tyler Alder <tyler.b.alder@gmail.com>
2020-05-10 08:13:09 -0700

    added authorization

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log('hello')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const MOVIEDEX = require("./moviedex.json");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
